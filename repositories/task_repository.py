90528e8573d601df27eb942fafae1495b5774e01
jakubnecessary <cabbage1991@gmail.com>
2021-07-06 20:36:31 +0100

    Homework Done

--- repositories/task_repository.py
+++ repositories/task_repository.py
@@ -1,56 +1,55 @@
 from db.run_sql import run_sql
-
 from models.task import Task
+from models.user import User
+import repositories.user_repository as user_repository
 
-def save(task):
-    sql = "INSERT INTO tasks (description, assignee, duration, completed) VALUES (%s, %s, %s, %s) RETURNING *"
-    values = [task.description, task.assignee, task.duration, task.completed]
-    results = run_sql(sql, values)
-    id = results[0]['id']
-    task.id = id
-    return task
-  
-  
-def select_all():  
-    tasks = [] 
+...
+
+def select_all():
+    tasks = []
 
     sql = "SELECT * FROM tasks"
     results = run_sql(sql)
 
     for row in results:
-        task = Task(row['description'], row['assignee'], row['duration'], row['completed'], row['id'] )
+        user = user_repository.select(row['user_id'])
+        task = Task(row['description'], user, row['duration'], row['completed'], row['id'] ) # MODIFIED
         tasks.append(task)
-    return tasks 
-    
+    return tasks
+
 
 def select(id):
     task = None
-    sql = "SELECT * FROM tasks WHERE id = %s"  
-    values = [id] 
+    sql = "SELECT * FROM tasks WHERE id = %s"
+    values = [id]
     result = run_sql(sql, values)[0]
-    
+
     if result is not None:
-        task = Task(result['description'], result['assignee'], result['duration'], result['completed'], result['id'] )
+        user = user_repository.select(result['user_id'])
+        task = Task(result['description'], user, result['duration'], result['completed'], result['id'] ) # MODIFIED
     return task
 
 
 def delete_all():
-    sql = "DELETE  FROM tasks" 
+    sql = "DELETE  FROM tasks"
     run_sql(sql)
 
+
 def delete(id):
-    sql = "DELETE  FROM tasks WHERE id = %s" 
+    sql = "DELETE  FROM tasks WHERE id = %s"
     values = [id]
     run_sql(sql, values)
 
 
 def update(task):
-    sql = "UPDATE tasks SET (description, assignee, duration, completed) = (%s, %s, %s, %s) WHERE id = %s"
-    values = [task.description, task.assignee, task.duration, task.completed, task.id]
-    run_sql(sql, values) 
-    
-    
-        
-    
-    
-        
+    sql = "UPDATE tasks SET (description, user_id, duration, completed) = (%s, %s, %s, %s) WHERE id = %s"
+    values = [task.description, task.user.id, task.duration, task.completed, task.id] 
+    run_sql(sql, values)
+
+def save(task):
+    sql = "INSERT INTO tasks (description, user_id, duration, completed) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [task.description, task.user.id, task.duration, task.completed]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    task.id = id
+    return task
